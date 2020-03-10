7ed7ed74de5f6f8ec6753ee89b3dc82474970b0a
David Haug <David.Haug@cudirect.com>
2020-03-10 15:25:07 -0700

    added components

---
+++ wwwroot/js/result-item.component.js
@@ -0,0 +1,64 @@
+Vue.component('result-item', {
+    props: ['item'],
+    data: function () {
+        return {
+          isExpanded: false,
+        }
+      },
+    template: `
+    <div>
+        <div class="row">
+            <div class="column column-10">
+                <div style="height: 75px; width: 75px; background-color: #ccc;"></div>
+            </div>
+            <div class="column">
+                <h3 class="wm-margin-bottom--0">{{ item.fullName }}</h3>
+                <span>{{ item.characterNames }}</span>
+            </div>
+            <div class="column column-20">
+                <span>Movies: {{ item.movieCount }}</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column">
+               <button v-on:click="isExpanded = !isExpanded" class="button button-clear wm-padding--0">
+                    <span v-if="!isExpanded"><i class="fa fa-chevron-right" aria-hidden="true"></i> Show details</span>
+                    <span v-else><i class="fa fa-chevron-down" aria-hidden="true"></i> Hide details</span>
+               </button>
+            </div>
+        </div>
+        <div class="row" v-if="isExpanded">
+            <div class="column column-offset-10">
+                <div v-for="character in item.characters" style="margin-bottom: 5px; margin-top: 5px;">
+                    <div class="row">
+                        <div class="column column-10">
+                            <div style="height: 40px; width: 40px; background-color: #ccc;"></div>
+                        </div>
+                        <div class="column">
+                            <h4 class="wm-margin-bottom--0">{{ character.fullName }}</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="column">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Movie Title</th>
+                                        <th>Release Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="movie in character.movies">
+                                        <td>{{ movie.title }}</td>
+                                        <td>{{ movie.releaseDate | moment }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+  })
--- wwwroot/js/result-list.component.js
+++ wwwroot/js/result-list.component.js
@@ -1,52 +1,16 @@
 Vue.component('result-list', {
-    props: ['actors'],
+    props: ['results'],
     template: `
-    <!--results-->
-    <div class="wm-margin-top--20">
-        <h4 v-if="!actors || actors.length == 0">Nope.</h4>
+    <div class="wm-margin-top--20" v-if="results">
+        <h4 v-if="results.length == 0">Nope.</h4>
         <div v-else>
-            <h4>{{ actors.length }} results</h4>
+            <h4>{{ results.length }} results</h4>
             <ul class="wm-list-style--none">
-                <li v-for="actor in actors" class="wm-margin-v--10" style="margin-left: -40px;">
-                    <div class="row">
-                        <div class="col-2">
-                            <div style="width: 75px; height: 75px; background-color: #ccc;"></div>
-                        </div>
-                        <div class="col-8">
-                            <h4>{{ actor.fullName }}</h4>
-                            <span>{{ actor.characterNames }}</span>
-                        </div>
-                        
-                        <div class="col-2">
-                            <span class="badge badge-pill badge-info">Movies: {{ actor.movieCount }}</span>
-                        </div>
-                    </div>
-                    <div class="row" v-if="actor.characters && actor.characters.length > 0">
-                        <div class="col-sm table-responsive">
-                            <h5>Movies</h5>
-                            <table class="table table-striped table-sm">
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Title</th>
-                                        <th>Release Date</th>
-                                        <th>Character</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="movie in actor.movies">
-                                        <td><div style="width: 50px; height: 50px; background-color: #ccc;"></div></td>
-                                        <td>{{ movie.title }}</td>
-                                        <td>{{ movie.releaseDate | moment }}</td>
-                                        <td>{{ movie.characterName }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                <li v-for="actor in results" class="wm-margin-v--10 wm-padding-v--10 wm-border-v">
+                    <result-item  v-bind:item="actor"></result-item>
                 </li>
             </ul>
         </div>
     </div>
-    <!--results end-->`
+    `
   })
